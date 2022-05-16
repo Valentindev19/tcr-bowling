0754632f25b828034c1c5fef7b7968cca3abd5bd
s_et-taleby <salah-eddine.et-taleby@infotel.com>
2022-05-16 11:10:07 +0200

    Création des classes Partie et Tests

---
+++ BowlingGame.Test/Tests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace BowlingGame.Test
+{
+	public class Tests
+    {
+        [TestMethod]
+        public void MethodeExemple()
+        {
+            var partie = new Partie();
+            partie.TesterPartie();
+
+            Assert.AreEqual(0, 87);
+        }
+    }
+}
